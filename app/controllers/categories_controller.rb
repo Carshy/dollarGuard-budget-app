9b4926695663d7173dc79a2fab7850c79618e492
Collins <mussocollins@gmail.com>
2022-11-23 21:32:24 +0300

    Add controllers and views for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,28 @@
 class CategoriesController < ApplicationController
   def index
+    @categories = Category.all
   end
 
   def show
   end
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    @category.user = current_user
+
+    if @category.save
+      flash[:success] = 'Category Added Successfully'
+      redirect_to new_category_path
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :amount, :icon)
   end
 end
